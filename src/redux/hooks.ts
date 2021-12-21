a41dece15611b46e30c921a0a57660417a3cb012
setamyDG <setamy.dg@gmail.com>
2021-12-21 15:23:36 +0100

    fix: delete unused code

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,4 @@
-import { Dispatch } from 'react';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { AppDispatch, RootState } from './store';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import type { RootState } from './store';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch = (): Dispatch<AppDispatch> => useDispatch<AppDispatch>();
