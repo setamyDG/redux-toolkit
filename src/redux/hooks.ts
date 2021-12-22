4243d7d4e5ac50b2fdad3b1e92442d0c33720b6b
setamyDG <setamy.dg@gmail.com>
2021-12-22 15:41:16 +0100

    fix: delete usused code

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,5 +1,4 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import type { RootState } from './store';
 
-// comment
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
